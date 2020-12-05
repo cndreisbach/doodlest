9bb010135d264adacd48a4e7af18a00fc42c17da
Clinton Dreisbach <clinton@dreisbach.us>
2020-12-05 15:50:57 -0500

    Remove save from keybindings

--- src/renderer/keybindings.js
+++ src/renderer/keybindings.js
@@ -1,11 +1,10 @@
 import Mousetrap from 'mousetrap'
 import { incPenSize, decPenSize, clearCanvas, useDrag, useLines } from './stores'
 
-export function setupKeyBindings ({ save }) {
+export function setupKeyBindings () {
   Mousetrap.bind('backspace', clearCanvas)
   Mousetrap.bind('mod+.', incPenSize)
   Mousetrap.bind('mod+,', decPenSize)
-  Mousetrap.bind('mod+s', save)
   Mousetrap.bind('alt', () => useDrag(true), 'keydown')
   Mousetrap.bind('alt', () => useDrag(false), 'keyup')
   Mousetrap.bind('shift', () => useLines(true), 'keydown')
